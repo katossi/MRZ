a2bfcd7848d6bac0eeba80f725167d8de1d2d242
Juarez <guillermo.juarez@sap.com>
2017-09-04 20:11:57 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { BrowserModule } from '@angular/platform-browser';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
   declarations: [
@@ -13,8 +11,8 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,9 +20,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Camera
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,103 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { NavController, ActionSheetController, LoadingController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+import Tesseract from 'tesseract.js'
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  srcImage: string;
+  OCRAD: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController,
+    public loadingCtrl: LoadingController,
+    public camera : Camera
+  ) {}
 
+  presentActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Choose Photo',
+          handler: () => {
+            this.getPicture(0); // 0 == Library
+          }
+        },{
+          text: 'Take Photo',
+          handler: () => {
+            this.getPicture(1); // 1 == Camera
+          }
+        },{
+          text: 'Demo Photo',
+          handler: () => {
+            this.srcImage = 'assets/img/demo.png';
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  getPicture(sourceType: number) {
+    // You can check the values here:
+    // https://github.com/driftyco/ionic-native/blob/master/src/plugins/camera.ts
+    
+    
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: 1, // DATA_URL
+      sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.srcImage = `data:image/jpeg;base64,${imageData}`;
+    }, (err) => {
+      console.log(`ERROR -> ${JSON.stringify(err)}`);
+    });
+  }
+
+  analyze() {
+    
+    //var text;
+
+    //let loader = this.loadingCtrl.create({
+    // content: 'Please wait...'
+    //});
+    //loader.present();
+
+
+       
+  Tesseract.create({
+    langPath: "https://groups.google.com/group/tesseract-ocr/attach/855081ee89d03/ocrb.traineddata"
+    }).recognize(document.getElementById('image'), "ocrb")  
+    .progress((progress) => {
+        console.log('progress', progress);
+    })
+    .then((tesseractResult) => {
+       console.log(tesseractResult);
+            });
+
+    
+    //(<any>window).OCRAD(document.getElementById('image'), text => {
+     // loader.dismissAll();
+     // alert(text);
+    // console.log(text);
+    //});
+  }
+
+  restart() {
+    this.srcImage = '';
+    this.presentActionSheet();
   }
 
 }
